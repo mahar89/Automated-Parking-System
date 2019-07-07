e2b1a43d6dd9146196116a35734a99a74be3da32
mahar89 <rajatmahajan2015@yahoo.co.in>
2019-07-07 11:00:01 +0530

    Following changes includes:
    1.Updating gitignore
    2.Adding test cases for ParkingCommandsService
    3.Creating pom.xml

---
+++ src/main/java/com/ticketing/parkingsystem/service/ParkingCommandService.java
@@ -0,0 +1,17 @@
+package com.ticketing.parkingsystem.service;
+
+public class ParkingCommandService {
+
+  public int parkVehicle(String regNumber, String color) {
+    int availableSpotNumber = -1;
+    return availableSpotNumber;
+  }
+
+  public boolean createParkingLot(int size) {
+    return false;
+  }
+
+  public boolean leaveParkingSpot(int spotNumber) {
+    return false;
+  }
+}
---
+++ src/test/java/com/ticketing/parkingsystem/util/ParkingSystemTestUtil.java
@@ -0,0 +1,15 @@
+package com.ticketing.parkingsystem.util;
+
+public class ParkingSystemTestUtil {
+
+  public static final String VEHICLE_REG_NUMBER = "KA-01-BB-0001";
+  public static final String VEHICLE_COLOR = "white";
+
+  public static final int VALID_PARKING_SIZE = 6;
+
+  public static final int INVALID_PARKING_SIZE = Integer.MAX_VALUE;
+
+  public static final int INVALID_PARKING_SPOT = Integer.MAX_VALUE;
+
+  public static final int VALID_PARKING_SPOT = 1;
+}
